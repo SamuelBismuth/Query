b5a4f8a549577301a8928b24835eaf6e9269646f
cyberlab <sam101@hotmail.fr>
2019-05-15 13:59:06 +0300

    rearanging the files with clang format

--- TextQuery.h
+++ TextQuery.h
@@ -4,26 +4,27 @@
 #ifndef TEXTQUERY_H
 #define TEXTQUERY_H
 
+#include <fstream>
+#include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
-#include <map>
-#include <set>
-#include <fstream>
+
 #include "QueryResult.h"
 
 class QueryResult; // declaration needed for return type in the query function
 class TextQuery {
 public:
-	using line_no = std::vector<std::string>::size_type;
-	TextQuery(std::ifstream&);
-    QueryResult query(const std::string&) const; 
-    void display_map(); // debugging aid
+  using line_no = std::vector<std::string>::size_type;
+  TextQuery(std::ifstream &);
+  QueryResult query(const std::string &) const;
+  void display_map(); // debugging aid
 private:
-	// pointer to a vector of lines of the input file
-    std::shared_ptr<std::vector<std::string>> file; 
-    // maps each word to a pointer to the set of lines in which that word appears
-    std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;  
+  // pointer to a vector of lines of the input file
+  std::shared_ptr<std::vector<std::string>> file;
+  // maps each word to a pointer to the set of lines in which that word appears
+  std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
 };
 std::ostream &print(std::ostream &os, const QueryResult &qr);
 #endif
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
-a.out: main.cpp TextQuery.cpp Query.cpp TextQuery.h Query.h QueryResult.h
+all: main.cpp TextQuery.cpp Query.cpp TextQuery.h Query.h QueryResult.h
 	g++ -std=c++11 main.cpp TextQuery.cpp Query.cpp
+	./a.out phone.txt
 
+clean: 
+	rm a.out
